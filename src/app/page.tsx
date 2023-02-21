e814bdb64c20f7b5edeb59f9ee6ddd8c57952cdd
Daniel Gabriel <reelpscanal@gmail.com>
2023-02-21 01:02:41 -0300

    functions and api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,63 @@
+'use client'
+import axios from 'axios'
+import Image from 'next/image'
+import { useState } from 'react'
+
+interface User {
+  image: string
+  title: string
+}
+
 export default function Home() {
+  const [data, setData] = useState<User>()
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber())
+
+  const options = {
+    method: 'GET',
+    url: `https://imdb-top-100-movies.p.rapidapi.com/top${randomNumber}`,
+    headers: {
+      'X-RapidAPI-Key': 'b5b64085famshd1a45bf01935c88p1c06cbjsn6458edcf15a2',
+      'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
+    },
+  }
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1
+  }
+
+  function handleButtonClick() {
+    setRandomNumber(generateRandomNumber())
+    console.log(randomNumber, data)
+    fetchData()
+  }
+
+  async function fetchData() {
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data)
+        setData(response.data)
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+  }
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Home</h1>
+      {data ? (
+        <Image
+          src={data.image}
+          alt={'Image of movie'}
+          width={380}
+          height={562}
+        />
+      ) : (
+        <p>Carregando...</p>
+      )}
+      {data ? <h2>{data.title}</h2> : <p>Carregando...</p>}
+      <button onClick={handleButtonClick}>Buscar</button>
     </main>
   )
 }
